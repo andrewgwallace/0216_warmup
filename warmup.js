8c3b434c5259b45fc17c3e50f6fd1750758930cf
andrewgwallace <andrew.g.wallace@gmail.com>
2018-02-16 10:52:00 -0500

    Add list of movies to director object literal

---
+++ warmup.js
@@ -0,0 +1,24 @@
+var movies = [];
+
+movies.pop("Star Wars");
+movies.push("Requiem for a Dream");
+movies.push("The Wrestler");
+movies.push("Shawshank Redemption");
+movies.push("Jaws");
+movies.push("The Goonies");
+movies.push("Ready Player One");
+movies.push("Indiana Jones and the Last Crusade");
+
+
+// console.log(movies)
+// console.log(movies.pop());
+// console.log(movies.sort());
+// movies.reverse();
+// movies.toString();
+// movies.shift();
+
+var directors = {};
+
+directors.spielberg = movies;
+
+console.log(directors);
